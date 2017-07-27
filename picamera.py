dceb0368105e059d66b5a6d1cbc31f9a5eda37a0
worriedmedic <comical115@gmail.com>
2017-07-27 19:39:24 -0400

    Update picamera.py

--- picamera.py
+++ picamera.py
@@ -1,15 +1,15 @@
 #PiCamera Testbed
 
 from time import sleep
-from picamera import PiCamera
+import picamera
 import os.path
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import logging
 logging.basicConfig()
 
-def capture():
-	with PiCamera() as camera:
+while True:
+	with picamera.PiCamera() as camera:
 		now = datetime.datetime.now()
 		output_dir = '/home/pi/picamera/images/' + now.strftime("%Y-%m") + '/'
 		output_file = 'image' + now.strftime("%H-%M-%S") + '.jpg'
@@ -18,9 +18,4 @@ def capture():
 		camera.annotate_text = now.strftime("%Y-%m-%d %H:%M:%S")
 		camera.capture(output_dir + output_file, resize=(853, 480))
 		camera.capture('/home/pi/picamera/images/output.jpg')
-
-if (1):
-	scheduler = BlockingScheduler()
-	scheduler.add_job(capture, 'interval', seconds=30)
-	capture()
-	scheduler.start()
+		sleep(30)
