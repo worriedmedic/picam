ceb523922fa970a0bfb04909f2ec79bd22683acd
worriedmedic <comical115@gmail.com>
2017-07-27 21:08:11 -0400

    Update rpi_picamera.py

--- rpi_picamera.py
+++ rpi_picamera.py
@@ -9,20 +9,20 @@ import logging
 logging.basicConfig()
 
 def capture():
-	with picamera as camera:
+	with picamera.PiCamera() as camera:
 		now = datetime.datetime.now()
-		output_dir = '/home/pi/picam/images/' + now.strftime("%Y-%m") + '/'
+		output_dir = '/home/pi/images/' + now.strftime("%Y-%m") + '/'
 		output_file = 'image' + now.strftime("%H-%M-%S") + '.jpg'
 		camera.iso = 1600
 		camera.vflip = True
-		camear.hflip = True
+		camera.hflip = True
 		camera.annotate_text = now.strftime("%Y-%m-%d %H:%M:%S")
-		camera.capture('/home/pi/picam/images/output.jpg')
+		camera.capture('/home/pi/images/output.jpg')
 		camera.resolution = (853, 480)
 		camera.capture(output_dir + output_file)
 
 if (1):
 	scheduler = BlockingScheduler()
-	scheduler.add_job(capture, 'interval', seconds=30)
+	scheduler.add_job(capture, 'interval', minutes=5)
 	capture()
 	scheduler.start()
