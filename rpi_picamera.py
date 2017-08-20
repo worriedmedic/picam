c6a2b5dd49a4f9d3244a4629d21e88c5fb817279
worriedmedic <comical115@gmail.com>
2017-08-20 11:09:14 -0400

    Update rpi_picamera.py

--- rpi_picamera.py
+++ rpi_picamera.py
@@ -28,7 +28,7 @@ def directorycheck():
 
 def dropbox_update(output, output_dir):
 	try:
-		subprocess.call(["/usr/local/bin/dropbox_uploader.sh", "upload", "-q", "%s" %output, "/Programming/images/%s" %output_dir])
+		subprocess.call(["/usr/local/bin/dropbox_uploader.sh", "-q", "upload", "%s" %output, "/Programming/images/%s" %output_dir])
 	except Exception:
 		pass
 
